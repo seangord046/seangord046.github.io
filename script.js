34ff8df614bb95d23f66643e9f301fd8f363611c
seangord046 <seanclaude.gordon@vhhscougars.org>
2024-09-30 18:42:03 +0000

    removejava

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const header = document.querySelector('h1');
-    const x = (e.clientX - window.innerWidth / 2) / 20;
-    const y = (e.clientY - window.innerHeight / 2) / 20;
-    header.style.transform = `translate(${x}px, ${y}px)`;
-});
